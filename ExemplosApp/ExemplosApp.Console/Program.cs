6ee93136a5d6eff38f8ef4d9016c235df3f45fad
Lucas Denicol <lucassdenicol@gmail.com>
2018-03-10 11:25:27 -0300

    aula 10/03/2018

---
+++ ExemplosApp/ExemplosApp.Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ExemplosAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Meu APP\n\n");
+
+            var idade = 18;
+            var nomePessoa = "Lucas";
+            var nomeDaMae = "Joana";
+            var idadeDaMae = 40;
+
+            Console.WriteLine(string.Format("Nome: {0} - Idade: {1} / Mãe: {2} - Idade: {3}", nomePessoa, idade, nomeDaMae, idadeDaMae));
+        }
+    }
+}
